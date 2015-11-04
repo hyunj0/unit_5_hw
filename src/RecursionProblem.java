e2c3980b7ba483ab0e22ecd0811cbbcc49c9e382
hyunj0 <hyunj0@hotmail.com>
2015-11-04 17:57:18 -0500

    initial commit

---
+++ src/RecursionProblem.java
@@ -0,0 +1,28 @@
+public class RecursionProblem
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println(isEvenWord("abcdeffedcba"));
+        System.out.println(isEvenWord("abcd"));
+        System.out.println(isEvenWord("00006666"));
+    }
+
+    public static boolean isEvenWord(String word)
+    {
+        //base case
+        if (word.length() == 0) {
+            //is even
+            return true;
+        } else {
+            //do recursive step
+            int index = word.indexOf(word.charAt(0), 1);
+            if (index == -1) {
+                return false;
+            }
+            word = word.substring(1, index) + word.substring(index + 1, word.length());
+            System.out.println(word);
+            return isEvenWord(word);
+        }
+    }
+}
